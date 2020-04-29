edef1adb1ef4c8ccedcb0b4bed65dd39402227ae
Chunhai Yang <cyang21@mail.greenriver.edu>
2020-04-29 04:04:31 -0400

    home page added

--- index.php
+++ index.php
@@ -12,10 +12,10 @@ $f3 = Base::instance();
 
 //default route
 $f3->route('GET /',function(){
-    echo '<h1>It is raining today</h1>';
+    //echo '<h1>It is raining today</h1>';
 
-//    $view = new Template();
-//    echo $view->render('view/home.html');
+    $view = new Template();
+    echo $view->render('views/home.html');
 });
 
 
